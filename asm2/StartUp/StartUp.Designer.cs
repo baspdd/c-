38bb06a3b0b150ee6859cff5bf6cfed3ece5f81c
Phạm Đức Duy <duypdhe160308@fpt.edu.vn>
2022-10-05 17:25:49 +0700

    hehe

--- asm2/StartUp/StartUp.Designer.cs
+++ asm2/StartUp/StartUp.Designer.cs
@@ -3,12 +3,12 @@
     partial class StartUp
     {
         /// <summary>
-        ///  Required designer variable.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        ///  Clean up any resources being used.
+        /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -23,28 +23,53 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.progressBar1 = new System.Windows.Forms.ProgressBar();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label4 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // progressBar1
+            // pictureBox1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(254, 345);
-            this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(295, 23);
-            this.progressBar1.TabIndex = 0;
-            this.progressBar1.Value = 100;
+            this.pictureBox1.Location = new System.Drawing.Point(48, 213);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Gray;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.Snow;
+            this.label3.Location = new System.Drawing.Point(255, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(454, 99);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Với nhiều logo đẹp và ấn tượng về các CLB mà bạn yêu thích";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Gray;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.Snow;
+            this.label2.Location = new System.Drawing.Point(255, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(454, 25);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Giới thiệu các CLB bóng đá nổi tiếng :";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label1
             // 
@@ -52,45 +77,43 @@
             this.label1.BackColor = System.Drawing.Color.Gray;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label1.Location = new System.Drawing.Point(43, 37);
+            this.label1.Location = new System.Drawing.Point(45, 33);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(103, 25);
-            this.label1.TabIndex = 1;
+            this.label1.TabIndex = 6;
             this.label1.Text = "Đón xem :";
             // 
-            // label2
+            // progressBar1
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(283, 73);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(345, 25);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Giới thiệu các CLB bóng đá nổi tiếng :";
+            this.progressBar1.Location = new System.Drawing.Point(255, 310);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(454, 23);
+            this.progressBar1.TabIndex = 5;
+            this.progressBar1.Value = 100;
             // 
-            // label3
+            // timer1
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label3.Location = new System.Drawing.Point(228, 213);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(345, 25);
-            this.label3.TabIndex = 3;
-            this.label3.Text = "Giới thiệu các CLB bóng đá nổi tiếng :";
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
-            // pictureBox1
+            // label4
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(43, 256);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
-            this.pictureBox1.TabIndex = 4;
-            this.pictureBox1.TabStop = false;
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Gray;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.Color.Snow;
+            this.label4.Location = new System.Drawing.Point(255, 252);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(213, 25);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Xin chờ trong giây lát :";
             // 
             // StartUp
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(755, 407);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -105,11 +128,13 @@
         }
 
         #endregion
-        private ProgressBar progressBar1;
-        private Label label1;
-        private Label label2;
-        private Label label3;
+
         private PictureBox pictureBox1;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private ProgressBar progressBar1;
         private System.Windows.Forms.Timer timer1;
+        private Label label4;
     }
 }
--- asm2/StartUp/StartUp.cs
+++ asm2/StartUp/StartUp.cs
@@ -1,3 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
 namespace StartUp
 {
     public partial class StartUp : Form
@@ -7,6 +17,31 @@ namespace StartUp
             InitializeComponent();
         }
 
-        
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            //if (progressBar1.Value < 90)
+            //{
+            //    progressBar1.Value += 10;
+
+            //}
+            //else
+            //{
+            //    timer1.Enabled = false;
+            //    frmMain f = new frmMain();
+            //    f.Show();
+            //    this.Hide();
+
+            //}
+            progressBar1.Increment(20);
+            if (progressBar1.Value >= progressBar1.Maximum)
+            {
+                timer1.Enabled = false;
+                frmMain f = new frmMain();
+                this.Hide();
+                f.Show();
+
+            }
+        }
     }
 }
