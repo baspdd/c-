bdc31368872b90429c88c97e8b987c53451bfeb9
Phạm Đức Duy <duypdhe160308@fpt.edu.vn>
2022-10-11 15:45:13 +0700

    up

---
+++ Demo/winform_EF/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace winform_EF
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgCustomer = new System.Windows.Forms.DataGridView();
+            this.btnDel = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtProName = new System.Windows.Forms.TextBox();
+            this.cbProductID = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtInstock = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtImage = new System.Windows.Forms.TextBox();
+            this.cbCategory = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgCustomer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgCustomer
+            // 
+            this.dgCustomer.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgCustomer.Location = new System.Drawing.Point(52, 280);
+            this.dgCustomer.Name = "dgCustomer";
+            this.dgCustomer.RowTemplate.Height = 25;
+            this.dgCustomer.Size = new System.Drawing.Size(696, 184);
+            this.dgCustomer.TabIndex = 34;
+            // 
+            // btnDel
+            // 
+            this.btnDel.Location = new System.Drawing.Point(575, 232);
+            this.btnDel.Name = "btnDel";
+            this.btnDel.Size = new System.Drawing.Size(75, 23);
+            this.btnDel.TabIndex = 31;
+            this.btnDel.Text = "Delete";
+            this.btnDel.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(357, 232);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 30;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(172, 232);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 29;
+            this.btnAdd.Text = "ADD";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // txtProName
+            // 
+            this.txtProName.Location = new System.Drawing.Point(515, 27);
+            this.txtProName.Name = "txtProName";
+            this.txtProName.Size = new System.Drawing.Size(208, 23);
+            this.txtProName.TabIndex = 24;
+            // 
+            // cbProductID
+            // 
+            this.cbProductID.FormattingEnabled = true;
+            this.cbProductID.Location = new System.Drawing.Point(151, 27);
+            this.cbProductID.Name = "cbProductID";
+            this.cbProductID.Size = new System.Drawing.Size(208, 23);
+            this.cbProductID.TabIndex = 23;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(390, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 15);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "ProductName :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(52, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 15);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "ProductID";
+            // 
+            // txtInstock
+            // 
+            this.txtInstock.Location = new System.Drawing.Point(515, 100);
+            this.txtInstock.Name = "txtInstock";
+            this.txtInstock.Size = new System.Drawing.Size(208, 23);
+            this.txtInstock.TabIndex = 38;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(390, 103);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 15);
+            this.label1.TabIndex = 36;
+            this.label1.Text = "UnitsInstock";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(52, 103);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 15);
+            this.label4.TabIndex = 35;
+            this.label4.Text = "UnitPrice";
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(151, 100);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(208, 23);
+            this.txtPrice.TabIndex = 42;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(390, 173);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 15);
+            this.label5.TabIndex = 40;
+            this.label5.Text = "Category";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(52, 173);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(40, 15);
+            this.label6.TabIndex = 39;
+            this.label6.Text = "Image";
+            // 
+            // txtImage
+            // 
+            this.txtImage.Location = new System.Drawing.Point(151, 173);
+            this.txtImage.Name = "txtImage";
+            this.txtImage.Size = new System.Drawing.Size(208, 23);
+            this.txtImage.TabIndex = 43;
+            // 
+            // cbCategory
+            // 
+            this.cbCategory.FormattingEnabled = true;
+            this.cbCategory.Location = new System.Drawing.Point(515, 170);
+            this.cbCategory.Name = "cbCategory";
+            this.cbCategory.Size = new System.Drawing.Size(208, 23);
+            this.cbCategory.TabIndex = 44;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 493);
+            this.Controls.Add(this.cbCategory);
+            this.Controls.Add(this.txtImage);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtInstock);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dgCustomer);
+            this.Controls.Add(this.btnDel);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtProName);
+            this.Controls.Add(this.cbProductID);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgCustomer)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DataGridView dgCustomer;
+        private Button btnDel;
+        private Button btnUpdate;
+        private Button btnAdd;
+        private TextBox txtProName;
+        private ComboBox cbProductID;
+        private Label label3;
+        private Label label2;
+        private TextBox txtInstock;
+        private Label label1;
+        private Label label4;
+        private TextBox txtPrice;
+        private Label label5;
+        private Label label6;
+        private TextBox txtImage;
+        private ComboBox cbCategory;
+    }
+}
