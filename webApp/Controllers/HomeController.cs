7baff1352b4b02f778f592605997cba40b1a8211
Phạm Đức Duy <duypdhe160308@fpt.edu.vn>
2022-10-21 01:56:25 +0700

    up

--- webApp/Controllers/HomeController.cs
+++ webApp/Controllers/HomeController.cs
@@ -1,12 +1,48 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-
+using webApp.Models;
 namespace webApp.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Index(Student student)
+        {
+            // truyền dữ liệu
+            //C1 - viewData
+            ViewData["message"] = "Đây là cách sử dụng viewdata";
+            ViewData["data"] = new Student
+            {
+                code = "sv1",
+                name = "hehe",
+                mark = 5
+            };
+
+            //C2 - viewBag
+            ViewBag.MSG = "Đây là cách sử dụng viewBag";
+            ViewBag.Data2 = new Student("sv2","hihi",6);
+
+            // C3 - Model
+            return View(student);
+        }
+        public IActionResult Add()
         {
             return View();
+        }
+        static List<Student> list = new List<Student>();
+        [HttpPost]
+        public IActionResult Add(Student student)
+        {
+            if (ModelState.IsValid)//Tạo model thành công
+            {
+                list.Add(student);
+                ViewBag.Data = list;
+                return View("ListStudent",student);
+            }
+            return View();
+        }
+        public IActionResult Detail(string code)
+        {
+           Student student = list.FirstOrDefault(item=>item.code == code);
+            return RedirectToAction("Index",student);
         }
     }
 }
--- webApp/Models/Student.cs
+++ webApp/Models/Student.cs
@@ -1,9 +1,15 @@
-﻿namespace webApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace webApp.Models
 {
     public class Student
     {
+        [Required(AllowEmptyStrings = false,ErrorMessage ="String is not empty")]
         public string code { get; set; }
+        
+        [Required(AllowEmptyStrings = false, ErrorMessage = "String is not empty")]
         public string name { get; set; }
+        [Range(0,10)]
         public int mark { get; set; }
 
         public Student(string code, string name, int mark)
