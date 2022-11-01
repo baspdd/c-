432699e8788a8837a9440d6dc4feee27309d22b1
baspdd <duypdhe160308@fpt.edu.vn>
2022-11-01 12:30:39 +0700

    uo

---
+++ test/asm3/asm3/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using asm3.Models;
+namespace asm3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            using (MyDB3Context context = new MyDB3Context())
+            {
+                ViewBag.Out = context.Products.First();
+                ViewBag.Products = context.Products.ToList();
+            }
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(string id)
+        {
+            using (MyDB3Context context = new MyDB3Context())
+            {
+                ViewBag.Out = context.Products.FirstOrDefault(item=>item.ProductCode.Equals(id));
+                ViewBag.Products = context.Products.ToList();
+            }
+            return View();
+        }
+    }
+}
