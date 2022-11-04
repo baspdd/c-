49fc623587f90ed63bc9cfff6544aedf0913d164
baspdd <duypdhe160308@fpt.edu.vn>
2022-11-04 22:11:16 +0700

    up

---
+++ Note/New folder/timer.cs
@@ -0,0 +1,38 @@
+﻿private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            //if (progressBar1.Value < 90)
+            //{
+            //    progressBar1.Value += 10;
+
+            //}
+            //else
+            //{
+            //    timer1.Enabled = false;
+            //    frmMain f = new frmMain();
+            //    f.Show();
+            //    this.Hide();
+
+            //}
+            progressBar1.Increment(10);
+            if (progressBar1.Value >= progressBar1.Maximum)
+            {
+                timer1.Enabled = false;
+                frmMain f = new frmMain();
+                this.Hide();
+                f.Show();
+
+            }
+        }
+
+ pictureBox1.Image = Image.FromFile(@"..\\..\\..\\pic\\ba.png");
+//get image web
+ string img = dgCustomer.Rows[e.RowIndex].Cells[4].FormattedValue.ToString();
+            WebRequest request = WebRequest.Create(img);
+            using (var response = request.GetResponse())
+            {
+                using (var str = response.GetResponseStream())
+                {
+                    pic.Image = Bitmap.FromStream(str);
+                }
+            }
