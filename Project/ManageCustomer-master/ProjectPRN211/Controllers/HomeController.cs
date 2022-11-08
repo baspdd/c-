f68432f7ab843c344c792fc6f49f56179f8a36bb
Phạm Đức Duy <duypdhe160308@fpt.edu.vn>
2022-11-08 15:56:10 +0700

    uo

--- Project/ManageCustomer-master/ProjectPRN211/Controllers/HomeController.cs
+++ Project/ManageCustomer-master/ProjectPRN211/Controllers/HomeController.cs
@@ -20,20 +20,47 @@ namespace ProjectPRN211.Controllers
             }
         }
 
-        public IActionResult Shop()
+        public IActionResult Shop(int id)
         {
             using (ASMSSContext context = new ASMSSContext())
             {
                 List<Product> products = context.Products.ToList();
-                List<Category> cate = context.Categories.ToList();
-                ViewBag.Cate1 = cate[0];
-                ViewBag.Cate2 = cate[1];
 
-                ViewBag.ListP = context.Products.ToList();
+                if (id >0)
+                {
+                    products = context.Products.Where(p => p.Type == id).ToList();
+                }
+
+                List<Category> cate = context.Categories.ToList();
+                ViewBag.Cate = cate;
+                ViewBag.ListP = products;
                 return View();
             }
         }
 
+        public IActionResult Search(string txt)
+        {
+            using (ASMSSContext context = new ASMSSContext())
+            {
+                List<Product> products = context.Products.Where(p=>p.Title.ToLower().Contains(txt.ToLower())).ToList();
+                List<Category> cate = context.Categories.ToList();
+                ViewBag.Cate = cate;
+                ViewBag.ListP = products;
+                return View("Shop");
+            }
+        }
+        [HttpPost]
+        IActionResult Login(string name , string pass)
+        {
+            using (ASMSSContext context = new ASMSSContext())
+            {
+                User us = context.Users.FirstOrDefault(u=>u.Username.Equals(name, StringComparison.OrdinalIgnoreCase) && u.Password.Equals(pass, StringComparison.OrdinalIgnoreCase));
+                if (us!=null)
+                {
+                }
+                return Tasks();
+            }
+        }
 
     }
 }
--- Project/ManageCustomer-master/ProjectPRN211/Program.cs
+++ Project/ManageCustomer-master/ProjectPRN211/Program.cs
@@ -5,7 +5,7 @@ var app = builder.Build();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Tasks}/{id?}"
+    pattern: "{controller=Home}/{action=Shop}/{id?}"
     );
 
 app.UseStaticFiles();
