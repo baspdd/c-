f417ba7f5356a94a9688f83c6ccd68856c84f244
baspdd <duypdhe160308@fpt.edu.vn>
2023-05-21 22:26:55 +0700

    up

---
+++ Prn221-WPF/ManageCategoriesApp/ManageCategoriesApp/WindowManageCategories.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows;
+
+namespace ManageCategoriesApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        ManageCategories categories = new ManageCategories();
+        private void Window_Loaded(object sender, RoutedEventArgs e) => LoadCategories();
+        private void LoadCategories()
+        {
+            dgCategories.ItemsSource = categories.GetCategories();
+        }
+
+        private void btnInsert_Click(object sender, RoutedEventArgs e)
+        {
+            Category category = new Category(txtCategoryName.Text);
+            categories.InsertCategory(category);
+
+        }
+
+        private void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void dgCategories_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Prn221-WPF/PRN211_SE1618/ManageCategoriesApp/ADO.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Interop;
+
+namespace ManageCategoriesApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        ManageCategories categories = new ManageCategories();
+        private void Window_Loaded(object sender, RoutedEventArgs e) => LoadCategories();
+        private void LoadCategories()
+        {
+            dgCategories.ItemsSource = categories.GetCategories();
+        }
+
+        private void btnInsert_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var category = new Category(txtCategoryName.Text);
+                categories.InsertCategory(category);
+                LoadCategories();
+            }
+            catch (System.Exception ex)
+            {
+
+                MessageBox.Show(ex.Message, "Insert");
+            }
+        }
+
+        private void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var category = new Category(Int32.Parse(txtCategoryID.Text), txtCategoryName.Text);
+                categories.UpdateCategory(category);
+                LoadCategories();
+            }
+            catch (System.Exception ex)
+            {
+
+                MessageBox.Show(ex.Message, "Uodate");
+            }
+        }
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var category = new Category { CategoryID = Int32.Parse(txtCategoryID.Text) };
+                categories.DeleteCategory(category);
+                LoadCategories();
+            }
+            catch (System.Exception ex)
+            {
+
+                MessageBox.Show(ex.Message, "Uodate");
+            }
+        }
+
+        private void dgCategories_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
