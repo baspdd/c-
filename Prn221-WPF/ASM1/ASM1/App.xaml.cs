f0310a2a105975f877326f315b18807346cf2e4d
Phạm Đức Duy <duypdhe160308@fpt.edu.vn>
2023-06-03 09:35:09 +0700

    up

---
+++ Prn221-WPF/ASM1/ASM1/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using WPF;
+
+namespace ASM1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void OnStartUp(object sender, StartupEventArgs e)
+        {
+            //var window = serviceProvider.GetService<MainWindow>();
+            //Login.show
+        }
+    }
+}
---
+++ Prn221-WPF/ASM1/ASM1/Login.xaml.cs
@@ -0,0 +1,53 @@
+﻿using ASM1;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            String email = txtEmail.Text;
+            String pass = txtPassword.Password;
+
+            var conf = new ConfigurationBuilder().AddJsonFile("appsettings.json")
+                .Build().GetSection("Admin");
+
+            if (String.IsNullOrEmpty(email) || String.IsNullOrEmpty(pass))
+            {
+                MessageBox.Show("input");
+            }
+
+
+            if (email.Equals(conf["email"]) && pass.Equals(conf["pass"]))
+            {
+                Product product = new Product();
+                product.Show();
+                this.Close();
+                //this.Hide();
+            }
+
+        }
+    }
+}
