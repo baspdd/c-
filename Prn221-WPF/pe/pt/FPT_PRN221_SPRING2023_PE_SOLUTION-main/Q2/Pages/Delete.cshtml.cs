e201121f57b10ddf1e7febc2bdbb2fc90e856345
Phạm Đức Duy <duypdhe160308@fpt.edu.vn>
2023-07-14 08:31:08 +0700

    up

--- Prn221-WPF/pe/pt/FPT_PRN221_SPRING2023_PE_SOLUTION-main/Q2/Pages/Delete.cshtml.cs
+++ Prn221-WPF/pe/pt/FPT_PRN221_SPRING2023_PE_SOLUTION-main/Q2/Pages/Delete.cshtml.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using Q2.Model;
 
 namespace Q2.Pages
@@ -7,11 +8,17 @@ namespace Q2.Pages
     public class DeleteModel : PageModel
     {
         private PE_PRN221_Trial3Context context = new PE_PRN221_Trial3Context();
-        public void OnGet(int id)
+        public IActionResult OnGet(int id)
         {
-            var movie = context.Movies.FirstOrDefault(x => x.Id == id);
+            var movie = context.Movies
+                .Include(m => m.Genres)
+                .Include(m=>m.Stars)
+                .FirstOrDefault(x => x.Id == id);
+            movie.Genres.Clear();
+			movie.Stars.Clear();
             context.Movies.Remove(movie);
             context.SaveChanges();
+            return RedirectToPage("/Index");
         }
     }
 }
