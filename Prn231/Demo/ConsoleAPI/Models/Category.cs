a934b990382a69e28db3bd0f39d7be960296096e
Phạm Đức Duy <duypdhe160308@fpt.edu.vn>
2023-10-08 10:23:34 +0700

    up

---
+++ Prn231/Demo/ConsoleAPI/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleAPI.Models
+{
+    public partial class Category
+    {
+        public Category()
+        {
+        }
+
+        public int categoryId { get; set; }
+        public string? categoryName { get; set; }
+
+        public override string? ToString()
+        {
+            return categoryId + "\t" + categoryName ;
+        }
+    }
+}
---
+++ Prn231/Demo/ConsoleAPI/iAPI/ICategoryApi.cs
@@ -0,0 +1,22 @@
+﻿using ConsoleAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Refit;
+
+namespace ConsoleAPI.iAPI
+{
+    public interface ICategoryApi
+    {
+        [Get("")]
+        Task<List<Category>> GetCategories();
+
+        [Get("/id?id={id}")]
+        Task<Category> GetCategoryById(int id);
+
+        [Post("")]
+        Task<HttpResponseMessage> AddCategory(Category category);
+    }
+}
