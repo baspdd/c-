1179f923efc52786fc4c220c61ac1925d3dade04
Phạm Đức Duy <duypdhe160308@fpt.edu.vn>
2023-10-17 15:00:52 +0700

    up

--- Prn231/Project/web/Controllers/HomeController.cs
+++ Prn231/Project/web/Controllers/HomeController.cs
@@ -25,7 +25,11 @@ namespace web.Controllers
             return View();
         }
 
+        public IActionResult Login()
+        {
 
+            return View();
+        }
 
         public IActionResult Privacy()
         {
