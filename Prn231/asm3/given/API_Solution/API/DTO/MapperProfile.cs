ffe71a4c3707f8cd22e7d95df7ede5336a53eada
baspdd <duypdhe160308@fpt.edu.vn>
2023-10-23 21:10:52 +0700

    s

---
+++ Prn231/asm3/given/API_Solution/API/DTO/MapperProfile.cs
@@ -0,0 +1,18 @@
+﻿using API.Models;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.DTO
+{
+    public class MapperProfile:Profile
+    {
+        public MapperProfile()
+        {
+            CreateMap<Product, ProductDTO>();
+            CreateMap<Category, CategoryDTO>();
+        }
+    }
+}
